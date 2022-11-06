57d1e4bdecb4a08b38ffa85c3c073dc8d9020677
MarcFaussurier <faussurier.marc@icloud.com>
2022-11-06 14:40:53 +0100

    menu component

--- www/pages/tiktaktoe.js
+++ www/pages/tiktaktoe.js
@@ -217,29 +217,15 @@ class TikTakToePage {
         this.checkPathname();
     }
 
-    render() {
+    async render() {
         document.getElementsByTagName("body")[0].innerHTML =
             `
-     <link rel="stylesheet" href="css/tiktaktoe.css">
-    <script type="text/javascript" src="js/tiktaktoe.js"></script>
-    <table id="entete">
-        <tr>
-            <td id="btn_page0" class = "pages" ><a class="btn_page" href="/">Jeux</a></td>
-            <td id="btn_page1" class = "pages" onclick="showChat()">Chat</td>
-            <td id="btn_page1" class = "pages" onclick="showRooms()">Salles</td>
-            <td id="btn_page2" class = "pages" onclick="showChat()">Nouvelle partie</td>
-        </tr>
-    </table>
-    <div class="pages" style="margin:auto;" onclick="">
-        <br/>
-        <center style="color: white;">
-            <span class="pseudo0" id="pseudo0"></span> <span id="score0">0</span>
-            <span class="pseudo1" id="pseudo1"></span> <span id="score1">0</span>
-        </center>
-        <br/>
-    </div>
-</header>
-<body>
+    <style>
+        ${await (((await fetch('css/tiktaktoe.css'))).text())}
+    </style> 
+
+    {${new MenuComponent()}}
+    
     <div class="menu hide" >
         <button onclick="hideMenu()">X</button>
         <div class="menuitems">
@@ -255,51 +241,7 @@ class TikTakToePage {
             </center> 
         </div>
     </div>
-    <div class="rooms hide" >
-        <button onclick="hideRooms()">X</button>
-        <div class="menuitems">
-            <br/>
-            <center>
-                <h1>Salles</h1>
-                <br/>
-                <br/>
-                <ul id="rooms_list">
-                    <li> . . .</li>
-                </ul>
-            </center> 
-        </div>
-    </div>
-        <div class="chat hide" >
-            <button onclick="hideChat()">X</button>
-            <br/>
-            <center>
-                <h1>Messages</h1>
-            </center>
-            <div class="messages">
-                
-            </div>
-            <input type="text"/><button style="display:inline-block">ENVOYER</button>
-        </div>
-          <div class="square">
-            <table id="grille" border="1">
-                <tr id ="L1">
-                    <td id="L1C1" class ="Cases"></td>
-                    <td id="L1C2" class ="Cases"></td>
-                    <td id="L1C3" class ="Cases"></td>
-                </tr>
-                <tr id ="L2">
-                    <td id="L2C1" class ="Cases"></td>
-                    <td id="L2C2" class ="Cases"></td>
-                    <td id="L2C3" class ="Cases"></td>
-                </tr>
-                <tr id ="L3">
-                    <td id="L3C1" class ="Cases"></td>
-                    <td id="L3C2" class ="Cases"></td>
-                    <td id="L3C3" class ="Cases"></td>
-                </tr>
-            </table>
-          </div>
-       
+    
     <footer>
     </footer>
     <script>
