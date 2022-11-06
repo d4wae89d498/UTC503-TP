3f9ba89249aaa6f6a36735e2864d7fff6b28f806
MarcFaussurier <faussurier.marc@icloud.com>
2022-11-06 15:13:07 +0100

    better UID

--- www/js/guid.js
+++ www/js/guid.js
@@ -1,9 +1,4 @@
 function guid() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
+   return Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15);
 }
