be2bae1a2a057a9582e7ef020a8b206cae2c8bec
MarcFaussurier <faussurier.marc@icloud.com>
2022-11-16 22:10:42 +0100

    ref

--- js/guid.js
+++
@@ -1,4 +0,0 @@
-function guid() {
-   return Math.random().toString(36).substring(2, 15) +
-        Math.random().toString(36).substring(2, 15);
-}
--- js/webroot.js
+++
@@ -1,1 +0,0 @@
-const webRoot = window.location.pathname.substring(0,window.location.pathname.lastIndexOf("/")+1);
