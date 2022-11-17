e729d8de4453d728817a45da1536dce30d3ed8f7
MarcFaussurier <faussurier.marc@icloud.com>
2022-11-17 11:04:38 +0100

    link

--- client/components/topmenu.js
+++ client/components/topmenu.js
@@ -10,7 +10,7 @@ class TopMenu extends Component
             <center>
                 <table id="entete">
                     <tr>
-                        <td id="btn_page0" class = "pages" ><a class="btn_page" href="./">Jeux</a></td>
+                        <td id="btn_page0" class = "pages" ><a class="btn_page" href="history.go(-1)">Jeux</a></td>
                         <td id="btn_page1" class = "pages" onclick="get_component('${chat.uid}').show()">Chat</td>
                         <td id="btn_page1" class = "pages" onclick="get_component('${rooms.uid}').show()">Salles</td>
                         <td id="btn_page2" class = "pages" onclick="socket.send('NEW')">Nouvelle partie</td>
